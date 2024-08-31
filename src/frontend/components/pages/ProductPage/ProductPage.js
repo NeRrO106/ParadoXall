574b76c4d40f1b680363b1656f96729cc16270e4
NeRrO106 <andreilimit@yahoo.com>
2024-08-31 11:56:35 +0300

    Product page problem solved

--- src/frontend/components/pages/ProductPage/ProductPage.js
+++ src/frontend/components/pages/ProductPage/ProductPage.js
@@ -11,10 +11,10 @@ const ProductPage = ({ toggleCart }) => {
     const { addToCart } = useCart();
 
     useEffect(() => {
-        const fetchProduct = () => {
+        const fetchProduct = async() => {
             try {
-                const response = fetch('https://paradoxall-80370d8dd2e4.herokuapp.com/api/products/');
-                const data = response.json();
+                const response = await fetch('https://paradoxall-80370d8dd2e4.herokuapp.com/api/products/');
+                const data = await response.json();
                 const foundProduct = data.find(p => p.product_id === parseInt(productId));
                 setProduct(foundProduct);
             } catch (error) {
